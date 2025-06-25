d40b49660abdb9746109bef012fd53f6faa29dbf
lakshaybxt <bishtlakshay14@gmail.com>
2025-06-25 23:20:02 +0530

    My Initial commit

---
+++ src/main/java/com/saathi/saathi_be/SaathiBeApplication.java
@@ -0,0 +1,13 @@
+package com.saathi.saathi_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaathiBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaathiBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saathi/saathi_be/SaathiBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saathi.saathi_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaathiBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
