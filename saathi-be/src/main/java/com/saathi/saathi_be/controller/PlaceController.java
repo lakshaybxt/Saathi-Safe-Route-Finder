1dcf0aef7e74e792f7124236f42064a2ac0d001d
lakshaybxt <bishtlakshay14@gmail.com>
2025-07-01 21:58:47 +0530

    Added Place Controller

---
+++ saathi-be/src/main/java/com/saathi/saathi_be/controller/PlaceController.java
@@ -0,0 +1,11 @@
+package com.saathi.saathi_be.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class PlaceController {
+}
